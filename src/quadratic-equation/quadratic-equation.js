f38d1a1bb53f5aa35351ac5902e1e0dc6c8c8bef
Alexey Zelenovsky <hiper2d@gmail.com>
2018-09-21 21:04:46 -0400

    Made it beautiful

--- src/quadratic-equation/quadratic-equation.js
+++ src/quadratic-equation/quadratic-equation.js
@@ -1,19 +1,28 @@
+String.prototype.toNumber = function() {
+    return parseInt(this.replace(/\s+/gi, ''));
+};
+
 module.exports = function solveEquation(equation) {
     let quadraticToken = '* x^2';
+    let quadraticTokenIndex = equation.indexOf(quadraticToken);
     let token = '* x ';
-    let a = equation.substring(0, equation.indexOf(quadraticToken)).noSpaces();
-    let b = equation.substring(equation.indexOf(quadraticToken) + quadraticToken.length, equation.indexOf(token)).noSpaces();
-    let c = equation.substring(equation.indexOf(token) + token.length).noSpaces();
-    let d = b * b - 4 * a * c;
-    let x1 = ((-1 * b + Math.sqrt(d)) / 2 / a).rd();
-    let x2 = ((-1 * b - Math.sqrt(d)) / 2 / a).rd();
+    let tokenIndex = equation.indexOf(token);
+
+    let a = equation.substring(0, quadraticTokenIndex).toNumber();
+    let b = equation.substring(quadraticTokenIndex + quadraticToken.length, tokenIndex).toNumber();
+    let c = equation.substring(tokenIndex + token.length).toNumber();
+
+    let d = getDiscriminantRoot(a, b, c);
+    let x1 = getX(a, b, d);
+    let x2 = getX(a, b, -1 * d);
+
     return (x1 <= x2) ? [x1, x2] : [x2, x1];
 };
 
-String.prototype.noSpaces = function() {
-    return this.replace(/\s+/gi, '');
-};
+function getDiscriminantRoot(a, b, c) {
+    return Math.sqrt(b * b - 4 * a * c);
+}
 
-Number.prototype.rd = function() {
-    return Math.round(this);
-};
+function getX(a, b, d) {
+    return Math.round(((-1 * b + d) / 2 / a));
+}
