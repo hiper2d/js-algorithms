177b8108ea88aaa25b4b254290a588b98d6810e1
Alexey Zelenovsky <hiper2d@gmail.com>
2018-09-21 20:09:25 -0400

    Played with prototypes

--- src/quadratic-equation/quadratic-equation.js
+++ src/quadratic-equation/quadratic-equation.js
@@ -1,11 +1,19 @@
 module.exports = function solveEquation(equation) {
     let quadraticToken = '* x^2';
     let token = '* x ';
-    let a = equation.substring(0, equation.indexOf(quadraticToken)).replace(/\s+/gi, '');
-    let b = equation.substring(equation.indexOf(quadraticToken) + quadraticToken.length, equation.indexOf(token)).replace(/\s+/gi, '');
-    let c = equation.substring(equation.indexOf(token) + token.length).replace(/\s+/gi, '');
+    let a = equation.substring(0, equation.indexOf(quadraticToken)).noSpaces();
+    let b = equation.substring(equation.indexOf(quadraticToken) + quadraticToken.length, equation.indexOf(token)).noSpaces();
+    let c = equation.substring(equation.indexOf(token) + token.length).noSpaces();
     let d = b * b - 4 * a * c;
-    let x1 = Math.round((-1 * b + Math.sqrt(d)) / 2 / a);
-    let x2 = Math.round((-1 * b - Math.sqrt(d)) / 2 / a);
+    let x1 = ((-1 * b + Math.sqrt(d)) / 2 / a).rd();
+    let x2 = ((-1 * b - Math.sqrt(d)) / 2 / a).rd();
     return (x1 <= x2) ? [x1, x2] : [x2, x1];
+};
+
+String.prototype.noSpaces = function() {
+    return this.replace(/\s+/gi, '');
+};
+
+Number.prototype.rd = function() {
+    return Math.round(this);
 };
