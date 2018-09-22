91ec31996f7a94b5bb3523dc81eff07658b14010
Alexey Zelenovsky <hiper2d@gmail.com>
2018-09-22 19:49:50 -0400

    Implemented Smart Calculator task

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Some tricky tasks to learn JavaScript and just fo fun
 * [Quadratic equation](./src/quadratic-equation/): [original](https://github.com/yankouskia/quadratic-equation)
 * [Zeros](./src/zeros/): [original](https://github.com/yankouskia/zeros)
 * [Money Exchange](./src/money-exchange/): [original](https://github.com/SergeyKovalchuk/Money-Exchange)
-* [Brakets](./src/brakets/): [original](https://github.com/yankouskia/additional_5)
+* [Brakets](./src/brackets/): [original](https://github.com/yankouskia/additional_5)
--- src/smart-calculator/smart-calculator.js
+++ src/smart-calculator/smart-calculator.js
@@ -1,37 +1,144 @@
 class SmartCalculator {
 
     constructor(initialValue) {
-        let value = initialValue;
+        let numbers = [initialValue];
+        let operators = [];
 
-        this.getValue = () => 11;
+        this.getNumbers = function() {
+            return numbers;
+        };
+
+        this.setNumbers = (stack) => numbers = stack;
+        this.getOperators = () => operators;
+        this.setOperators = (stack) => operators = stack;
     }
 
     add(number) {
-        /*this.getNumbers().push(number);
-        this.getOperations().push('+');
-        */
+        this.getNumbers().push(number);
+        this.getOperators().push('+');
         return this;
     }
 
     subtract(number) {
+        this.getNumbers().push(number);
+        this.getOperators().push('-');
         return this;
     }
 
     multiply(number) {
+        this.getNumbers().push(number);
+        this.getOperators().push('*');
         return this;
     }
 
     devide(number) {
+        this.getNumbers().push(number);
+        this.getOperators().push('/');
         return this;
     }
 
     pow(number) {
+        this.getNumbers().push(number);
+        this.getOperators().push('^');
         return this;
     }
 
     toString() {
-        return this.getValue();
+        calculateOperationsInReversedOrder(this, ['^']);
+        calculateOperationsInDirectOrder(this, ['*', '/']);
+        calculateOperationsInDirectOrder(this, ['+', '-']);
+        return this.getNumbers()[0];
     }
 }
+
+function calculateOperationsInReversedOrder(calculator, operations) {
+    let numberStack = [];
+    let operationStack = [];
+
+    while (calculator.getNumbers().isNotEmpty()) {
+        numberStack.push(calculator.getNumbers().pop());
+        calculateIfPossible(numberStack, operationStack, operations, true);
+
+        if (calculator.getOperators().isNotEmpty()) {
+            operationStack.push(calculator.getOperators().pop());
+        }
+    }
+    calculator.setNumbers(numberStack.reverse());
+    calculator.setOperators(operationStack.reverse())
+}
+
+function calculateOperationsInDirectOrder(calculator, operations) {
+    let numberStack = [];
+    let operationStack = [];
+
+    while (calculator.getNumbers().isNotEmpty()) {
+        numberStack.push(calculator.getNumbers().shift());
+        calculateIfPossible(numberStack, operationStack, operations);
+
+        if (calculator.getOperators().isNotEmpty()) {
+            operationStack.push(calculator.getOperators().shift());
+        }
+    }
+    calculator.setNumbers(numberStack);
+    calculator.setOperators(operationStack)
+}
+
+function calculateIfPossible(numberStack, operationStack, operations, isReversedOperationOrder) {
+    if (
+        numberStack.length >= 2
+        && operationStack.length >= 1
+        && (isOperationIn(operationStack.lastItem(), operations))
+    ) {
+        performOperationOnStacks(numberStack, operationStack, isReversedOperationOrder);
+    }
+}
+
+function performOperationOnStacks(numberStack, operationStack, isReversedOperationOrder) {
+    let val2 = numberStack.pop();
+    let val1 = numberStack.pop();
+    let res = isReversedOperationOrder
+        ? performOperation(val2, val1, operationStack.pop())
+        : performOperation(val1, val2, operationStack.pop());
+
+    numberStack.push(res);
+}
+
+function performOperation(value1, value2, operation) {
+    return value1.applyOperation(operation, value2);
+}
+
+function isOperationIn(value, operations) {
+    return operations.find(op => op === value);
+}
+
+Number.prototype.applyOperation = function(operation, number) {
+    let result;
+    switch(operation) {
+        case '+':
+            result = this + number;
+            break;
+        case '-':
+            result = this - number;
+            break;
+        case '*':
+            result = this * number;
+            break;
+        case '/':
+            result = this / number;
+            break;
+        case '^':
+            result = Math.pow(this, number);
+            break;
+    }
+    return result;
+};
+
+Array.prototype.lastItem = function() {
+    return this[this.length - 1];
+};
+
+Array.prototype.isNotEmpty = function() {
+    return this.length !== 0;
+};
 
 module.exports = SmartCalculator;
